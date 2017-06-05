8e7cc02bf8a0429dc89d527317654ec9d3466f3f
Albert Wigmore <albert@albertwigmore.co.uk>
2017-06-05 12:13:15 +0100

    Implemented LaunchQuery API

--- LaunchQuery.py
+++ LaunchQuery.py
@@ -5,79 +5,42 @@ from pprint import pprint
 from datetime import datetime, timedelta
 
 
-class LaunchQuery:
-    def __init__(self):
+class LaunchQuery(object):
+    def __init__(self, *args, **kwargs):
         self.url_base = 'https://launchlibrary.net/1.2/'
 
-    def agency(self):
-        pass
+    def __getattr__(self, method_name):
+        if method_name in ['agency', 'agencytype', 'calendar', 'eventtype',
+                           'launch', 'launchevent', 'launchstatus', 'location',
+                           'mission', 'missionevent', 'missiontype', 'pad',
+                           'rocket', 'rocketevent', 'rocketfamily']:
+            def get(*args, **kwargs):
+                r = requests.get(self.url_base + method_name, params=kwargs)
+                return r.json()
+            return get
+        else:
+            raise AttributeError()
 
-    def agencytype(self):
-        pass
-
-    def calendar(self):
-        pass
-
-    def eventtype(self):
-        pass
-
-    def launch(self, now, dur):
+    def launch2(self, now, dur):
         current = now.strftime('%Y-%m-%d')
         next = (now + timedelta(days=dur)).strftime('%Y-%m-%d')
         r = requests.get(self.url_base + 'launch/' + current + '/' + next)
         return r.json()
 
-    def launchevent(self):
-        pass
-
-    def launchstatus(self):
-        pass
-
-    def location(self):
-        pass
-
-    def mission(self):
-        pass
-
-    def missionevent(self):
-        pass
-
-    def missiontype(self):
-        pass
-
-    def pad(self):
-        pass
-
-    def rocket(self):
-        pass
-
-    def rocketevent(self):
-        pass
-
-    def rocketfamily(self):
-        pass
-
 
 if __name__ == '__main__':
     c = LaunchQuery()
-    data = c.launch(datetime.now(), 7)
-    for x in data['launches']:
-        # pprint(x)
-        print(x['name'])
-        # print(x['rocket']['familyname'])
-        print(x['rocket']['agencies'][0]['name'])
-        print(x['location']['name'])
-        print(x['net'])
-
-        launch_time = datetime.strptime(x['net'], '%B %d, %Y %H:%M:%S UTC')
-        x = True
-        while x:
-            s = (launch_time - datetime.now()).total_seconds()
-            if s < 0:
-                x = False
-                continue
-            days, remainder = [int(z) for z in divmod(s, 86400)]
-            hours, remainder = [int(z) for z in divmod(remainder, 3600)]
-            minutes, seconds = [int(z) for z in divmod(remainder, 60)]
-            print '\r' + str(days) + ' days, ' + str(hours) + ':' + str(minutes) + ':' + str(seconds)
-            time.sleep(1)
+    data = c.launch(next=1)
+    launch_time = datetime.strptime(data['launches'][0]['net'], '%B %d, %Y %H:%M:%S UTC')
+    print(launch_time)
+    x = True
+    while x:
+        s = (launch_time - datetime.now()).total_seconds()
+        if s < 0:
+            x = False
+            continue
+        days, remainder = [int(z) for z in divmod(s, 86400)]
+        hours, remainder = [int(z) for z in divmod(remainder, 3600)]
+        minutes, seconds = [int(z) for z in divmod(remainder, 60)]
+        print '\r' + str(days) + ' days, ' + str(hours) + ':' + str(minutes) + ':' + str(seconds)
+        time.sleep(1)
